4539cd4ca4ca920f0c5dd345c75de3e7db132ef8
lisacandemyr <141151710+lisacandemyr@users.noreply.github.com>
2023-11-23 15:27:59 +0000

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -2,5 +2,12 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+        <p>This project was coded by Lisa and is open-sourced on GitHub</p>
+      </div>
+    </div>
+  );
 }
