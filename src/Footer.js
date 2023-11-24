399c5aa7b07cb1a5d10c851dccd5d4c05b9366e8
lisacandemyr <141151710+lisacandemyr@users.noreply.github.com>
2023-11-24 16:52:07 +0000

    added link to GitHub repository

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,14 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <p className="Footer">
-      This project was coded by Lisa and is open-sourced on GitHub
+      This project was coded by Lisa and is{" "}
+      <a
+        href="https://github.com/lisacandemyr/weather-app-react"
+        target="_blank"
+        rel="noreferrer"
+      >
+        open-sourced on GitHub
+      </a>
     </p>
   );
 }
